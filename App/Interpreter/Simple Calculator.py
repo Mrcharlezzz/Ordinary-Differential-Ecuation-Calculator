99c7a67aedb370e471a2c5eaeed214ea5b5a7b98
abg555 <boueadriana2@gmail.com>
2025-02-08 13:09:02 -0500

    least squares method
    The method of least squares is already done, it was carried out for an approximation of a linear function and for a parabola, in addition to giving the errors with respect to each function

--- App/Interpreter/Simple Calculator.py
+++
@@ -1,8 +0,0 @@
-from math import sin, cos, tan, log, sqrt,pi
-
-def simple(A):
-    resultado = eval(A, {"__builtins__": None}, {"sin": sin, "cos": cos, "tan": tan, "ln": log, "log":logs, "sqrt": sqrt, "pi": pi}) # eval() evaluates the mathematical expression
-    return round(resultado,10)   # round the result to 10 decimal places
-    
-def logs(base,argumento):              # method to achieve the base change, go from ln to log
-    return log(argumento)/log(base)
